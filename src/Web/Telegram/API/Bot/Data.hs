d25dfce3009dad82ad76197da4fe2c11f432ccba
DrewFenwick <drew.e.fenwick+github@gmail.com>
2020-09-01 15:24:48 +0100

    Reimplement Update partial funcs
    Reimplemented in order to maintain compatability.

--- src/Web/Telegram/API/Bot/Data.hs
+++ src/Web/Telegram/API/Bot/Data.hs
@@ -87,6 +87,15 @@ module Web.Telegram.API.Bot.Data
     , inlineQueryResultCachedVoice
     , inputMediaPhoto
     , inputMediaVideo
+    , message
+    , edited_message
+    , channel_post
+    , edited_channel_post
+    , inline_query
+    , chosen_inline_result
+    , callback_query
+    , shipping_query
+    , pre_checkout_query
     ) where
 
 import           Prelude                      hiding (id)
